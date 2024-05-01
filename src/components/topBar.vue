d5ded84f61c5e52fc9210cb3301d672fe3a49ac6
p587233200 <73016258+p587233200@users.noreply.github.com>
2024-05-02 02:38:58 +0800

    fix: topbar 排版 and Alert,Auth

--- src/components/topBar.vue
+++ src/components/topBar.vue
@@ -1,67 +1,59 @@
 <template>
+    <v-alert v-if="alertStore.show" :type="alertStore.type" title="成功～">{{ alertStore.text }}</v-alert>
     <v-app-bar color="blue-lighten-4">
         <v-toolbar-title>
             <v-avatar size="80">
-                <img :src="topBar_bird"  class="logo" alt="Logo" @click="backMain" />
+                <img :src="topBar_bird" class="logo" alt="Logo" @click="backMain" />
             </v-avatar>
         </v-toolbar-title>
         <v-row>
             <v-col class="d-flex justify-end" cols="12">
-                <!-- <v-row class="d-flex justify-end mr-3">
-                     <a class="mr-5 d-flex align-center"></a>
-                    <v-btn
-                        variant="outlined"
-                        class="mr-2"
-                        color="rgba(70, 105,147, 1)"
-                        @click="admin"                      
-                    >
-                        管理員
+                <v-row v-if="authStore.login_status" class="d-flex justify-end mr-3">
+                    <a class="mr-5 d-flex align-center"></a>
+                    <v-btn color="rgba(70, 105,147, 1)" prepend-icon="mdi-account-circle" @click="admin" class="mr-2" style="font-size:18px;">
+                        <template v-slot:prepend>
+                            <v-icon color="success"></v-icon>
+                        </template>
+                        會員帳號：{{authStore.username }}
                     </v-btn>
-                    <v-btn
-                        variant="outlined"
-                        color="rgba(70, 105,147, 1)"
-                        @click="logout"
-                    >
+                    <v-btn variant="outlined" color="rgba(70, 105,147, 1)" prepend-icon="mdi-logout"
+                        @click="logout" class="mr-2" style="font-size:18px;">
                         登出
+                        <template v-slot:prepend>
+                            <v-icon color="warning"></v-icon>
+                        </template>
                     </v-btn>
-                </v-row>  -->
-                <v-row class="d-flex justify-end mr-3">
-                     <v-btn 
-                        variant="outlined"
-                        color="rgba(70, 105,147, 1)"
-                        prepend-icon="mdi-account-circle"
-                        @click="sendLogin"
-                    >
-                    <template v-slot:prepend>
-                        <v-icon color="success"></v-icon>
-                    </template>
+                </v-row>
+
+                <v-row v-else='authStore.login_status' class="d-flex justify-end mr-3">
+                    <v-btn variant="outlined" color="rgba(70, 105,147, 1)" prepend-icon="mdi-account-circle"
+                        class="mr-2" style="font-size:18px;" @click="sendLogin">
+                        <template v-slot:prepend>
+                            <v-icon color="success"></v-icon>
+                        </template>
                         登入
                     </v-btn>
-                    <v-btn  variant="outlined"
-                     color="rgba(70, 105,147, 1)"
-                     prepend-icon="mdi-account-plus-outline"
-                     @click="sendRegister">
-                     <template v-slot:prepend>
-                        <v-icon color="warning"></v-icon>
-                    </template>
+                    <v-btn variant="outlined" color="rgba(70, 105,147, 1)" prepend-icon="mdi-account-plus-outline"
+                        class="mr-2" style="font-size:18px;" @click="sendRegister">
+                        <template v-slot:prepend>
+                            <v-icon color="warning"></v-icon>
+                        </template>
                         註冊
                     </v-btn>
                 </v-row>
             </v-col>
-        </v-row> 
+        </v-row>
     </v-app-bar>
 </template>
 
 <script setup>
 import topBar_bird from '@/assets/topBar_bird.png';
 import { useRouter } from 'vue-router';
-// import { useRouter } from '@/router';
 // import { computed, onBeforeMount } from 'vue';
-// import { useAuthStore } from '@/store/Auth.js';
-// import { useAlertStore } from '@/store/Alert.js';
-// const authStore = useAuthStore();
-// const alertStore = useAlertStore();
-// const route = useRoute();
+import { useAuthStore } from '@/services/Auth.js';
+import { useAlertStore } from '@/services/Alert.js';
+const authStore = useAuthStore();
+const alertStore = useAlertStore();
 const router = useRouter();
 // const loginButton = computed(() => route.name === 'login');
 
@@ -77,11 +69,11 @@ function sendRegister() {
 //     router.push('/admin/posts');
 // }
 
-// function logout() {
-//     backMain();
-//     authStore.removeAuth();
-//     alertStore.callAlert('登出!');
-// }
+function logout() {
+    backMain();
+    authStore.removeAuth();
+    alertStore.callAlert('登出!!');
+}
 
 function backMain() {
     router.push('/');
@@ -94,7 +86,9 @@ function backMain() {
 </script>
 <style>
 .logo {
-  width: 80px; /* 調整圖片的寬度 */
-  height: auto; /* 讓高度按比例自動調整 */
+    width: 80px;
+    /* 調整圖片的寬度 */
+    height: auto;
+    /* 讓高度按比例自動調整 */
 }
 </style>
